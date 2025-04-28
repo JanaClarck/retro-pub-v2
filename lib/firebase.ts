ad80e6bc1910e92a3bd407801db07235295ea4cc
JanaClarck <larg.sapetskiy@yahoo.com>
2025-04-28 15:36:08 +0200

    fix: firebase import path

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -6,9 +6,9 @@ import { initializeApp, getApps, getApp } from "firebase/app";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_AUTH_DOMAIN",
-  projectId: "YOUR_PROJECT_ID",
+  apiKey: "AIzaSyCedDX51JOmjeUANdJPiKDU4Sof9wZGph0",
+  authDomain: "retropub-7bfe5.firebaseapp.com",
+  projectId: "retropub-7bfe5",
   storageBucket: "retropub-7bfe5.appspot.com",
   messagingSenderId: "14470383176",
   appId: "1:14470383176:web:a4588666865532f72a7a7d",

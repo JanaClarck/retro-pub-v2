8f9e3407f183e9e9ae791dbce86b67e92d5f4286
JanaClarck <larg.sapetskiy@yahoo.com>
2025-04-28 16:16:00 +0200

    fix: firebase import path

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -17,5 +17,7 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
+const auth = getAuth(app);
+const db = getFirestore(app);
 
-export default app;
+export { app, auth, db };
